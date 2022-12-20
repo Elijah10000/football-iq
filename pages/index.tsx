57f9acb20f78f3959e8f621818a112e9143595fb
Elijah McNamara <78796236+Elijah10000@users.noreply.github.com>
2022-12-20 12:10:44 +0000

    Dropdown menu on home page (#2)
    * updates
    
    * made-edits-to-player-layouts
    
    * api-and-styling
    
    * changes
    
    * tidy up from merge conflicts
    
    Co-authored-by: Elijah2609 <56544041+Elijah2609@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,11 @@ import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { playersApi } from '../api/players'
+import { leaguesApi } from 'api/leagues'
 import styled from 'styled-components'
-import { Container, PlayersList, Player, TeamCrest, PlayerStatsList } from 'styles/index'
+import { Container, PlayersList, Player, TeamCrest, PlayerStatsList, LogoDiv, LeagueTextDiv, LeaguesList, LeagueStatsList, League, LeagueDiv, LeagueNames } from 'styles/index'
+import { useState, useEffect } from 'react'
+import AsyncSelect from 'react-select/async';
 
 type team = {
   id: number;
@@ -20,23 +23,44 @@ type player = {
   photo: string;
 }
 
+type league = {
+  league: {
+    id: number;
+    name: string;
+    type: string;
+    logo: string;
+  },
+
+  country: {
+    name: string,
+    code?: string,
+    flag?: string
+  }
+}
+
 type IHome = {
   players: player[];
   team: team;
+  leagues: league[];
 }
 
 
-
-export default function Home({ players, team }: IHome) {
+export default function Home({ players, team, leagues }: IHome) {
   return (
     <Container>
 
-      <TeamCrest>
-        <img src={team.logo} alt="ff" />
-      </TeamCrest>
+      <LogoDiv>
+        <div>
+          <h1 style={{ borderBottom: '4px solid black' }}>
+            Football IQ
+          </h1>
+        </div>
+        <TeamCrest>
+          <img src={team.logo} alt="ff" />
+        </TeamCrest>
+      </LogoDiv>
 
       <PlayersList>
-
         {players.map((player: player) => {
           return (
             <Player key={player.id}>
@@ -51,17 +75,76 @@ export default function Home({ players, team }: IHome) {
           );
         })}
       </PlayersList>
+
+
+      <LeagueDiv>
+
+        <LeagueTextDiv>
+          <h1 style={{ borderBottom: '4px solid black' }}>
+            Leagues
+          </h1>
+        </LeagueTextDiv>
+
+        <LeaguesList>
+
+          {leagues.map((league: league) => {
+            const leaguesToReturn = [39, 78, 61, 135, 140, 2];
+
+            if (!leaguesToReturn.includes(league.league.id)) return;
+
+            return (
+              <League key={league.league.id}>
+                <LeagueStatsList>
+                  <LeagueNames>
+                    {`${league.league.name}`}
+                    <img src={league.league.logo} />
+                  </LeagueNames>
+                  {/* {`Logo: ${league.league.logo}`}</li> */}
+                </LeagueStatsList>
+              </League>
+            );
+          })}
+        </LeaguesList>
+
+      </LeagueDiv>
+
+
+
     </Container>
   )
 }
 
+// export function Dropdown (){
+//   const [items, setItems] = useState([]);
+//   const [inputValue, setValue] = useState ([]);
+//   const [selectedValue, setSelectedValue] = useState ([null]);
+
+//   const handleInputChange = value => {
+//     setValue(value);
+//   };
+
+//   const handleChange = value => {
+//     setSelectedValue(value);
+//   };
+
+//   const fetchData = () => {
+//     return  axios.get(`https://api-football-v1.p.rapidapi.com/v3/players/squads`).then(result => {
+//       const res = result.data.data;
+//       return res;
+//     });
+//   }
+// }
+
 export async function getServerSideProps(context: any) {
   const { data } = await playersApi.getPlayersBySquadId("49");
+  const leagues = await leaguesApi.getLeagues();
   console.log(data.response[0])
   return {
     props: {
       players: data.response[0].players,
-      team: data.response[0].team
-    }, // will be passed to the page component as props
+      team: data.response[0].team,
+      leagues: leagues.data.response
+    },
   }
 }
+
